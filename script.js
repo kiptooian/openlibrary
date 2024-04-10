82d0625e48063fa9c0a34718caa1503f005a26db
kiptooian <kiptooian12@gmail.com>
2024-04-10 20:52:58 +0300

    Limit and sorting

--- script.js
+++ script.js
@@ -1,22 +1,67 @@
+var loadedBooks = []
+var limit = 10
+
 function loadBooks(data) {
     let url = "https://openlibrary.org/search.json?"
 
-    if(data.query) url += `q=${data.query}`
+    if(data.query) {
+        url += `q=${data.query}`
+    } else {
+        url += `q=Classics`
+    }
 
-    url += "&limit=10&fields=key,title,author_name,editions,editions.key,editions.title,ratings_average"
+    if(limit) url += `&limit=${limit}`
+
+    url += "&fields=key,title,author_name,editions,editions.key,editions.title,ratings_average"
 
     showLoading()
 
     axios.get(url)
         .then(resp => {
             if(resp && resp.data && resp.data.docs) {
+                loadedBooks = resp.data.docs
                 renderBooks(resp.data.docs)
             }
         })
         .catch(err => {
             showError()
+            loadedBooks = []
             console.log(err)
         })
+}
+
+function sortBooks(value) {
+    if(value) {
+        let sortedBooks
+
+        if(value == 'title') {
+            sortedBooks = loadedBooks.sort((a, b) => {
+                const titleA = a.title.toLowerCase();
+                const titleB = b.title.toLowerCase();
+                if (titleA < titleB) return -1;
+                if (titleA > titleB) return 1;
+                return 0;
+            });
+        }
+
+        if(value == 'author') {
+            sortedBooks = loadedBooks.sort((a, b) => {
+                const authorA = a.author_name ? a.author_name.join(', ').toLowerCase() : ''
+                const authorB = b.author_name ? b.author_name.join(', ').toLowerCase() : ''
+                if (authorA < authorB) return -1;
+                if (authorA > authorB) return 1;
+                return 0;
+            });
+        }
+
+        if(value == 'rating') {
+            sortedBooks = loadedBooks.sort((a, b) => {
+                return b.ratings_average - a.ratings_average;
+            });
+        }
+
+        renderBooks(sortedBooks)
+    }
 }
 
 function showLoading() {
@@ -94,10 +139,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var searchInput = document.getElementById("search-input");
 
     searchInput.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        
-        loadBooks({query: searchInput.value})
-    }
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            
+            loadBooks({query: searchInput.value})
+        }
     });
+
+    //change event on sort
+    var sortInput = document.getElementById("sort");
+
+    sortInput.addEventListener("change", function(event) {
+        sortBooks(sortInput.value)
+    });
+
+    //limit
+    var limitInput = document.getElementById("limit");
+
+    limitInput.addEventListener("change", function(event) {
+        limit = limitInput.value
+
+        loadBooks({query: searchInput.value})
+    });
+
 });
